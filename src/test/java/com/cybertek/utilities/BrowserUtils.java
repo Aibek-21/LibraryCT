962f7d8d0bb589a9db845002f027a5c31b2f2f53
Aibek-21 <aibek61@gmail.com>
2021-02-19 18:06:38 -0800

    new project created 1

---
+++ src/test/java/com/cybertek/utilities/BrowserUtils.java
@@ -0,0 +1,49 @@
+package com.cybertek.utilities;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrowserUtils {
+
+    /*
+
+     */
+    public static List<String> getElementsText(List<WebElement> webElementList) {
+
+        //Placeholder empty list of web elements
+        List<String> webElementsAsString = new ArrayList<>();
+        //looping through list of web elements and storing text into placeholder list
+        for (WebElement each : webElementList) {
+            webElementsAsString.add(each.getText());
+        }
+        //returning final List<String>
+        return webElementsAsString;
+    }
+
+
+    //Method to assert title
+
+    public static void titleVerification(String expectedTitle) {
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue(actualTitle.equalsIgnoreCase(expectedTitle));
+
+    }
+
+
+    //create method name: wait
+    //converting milliseconds to seconds
+    public static void sleep(int second) {
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+            System.out.println("something happened in sleep method");
+
+        }
+    }
+
+}
