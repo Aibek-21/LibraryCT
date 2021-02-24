b73de912897a1f92503b2560629edb73d84a3f3c
Erjon Domi <erjondomi@hotmail.com>
2021-02-24 14:30:57 -0500

    finished my runner

--- src/test/java/com/cybertek/runners/ParallelTesting.java
+++ src/test/java/com/cybertek/runners/ParallelTesting.java
@@ -9,7 +9,10 @@ import org.junit.runner.RunWith;
 
         plugin = {"html:target/cucumber-report.html",
                 "json:target/cucumber.json",
-                "rerun:target/rerun.txt"
+                "rerun:target/rerun.txt",
+                "pretty",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "timeline:test-output-thread/"
         },
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions",
