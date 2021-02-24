672496e980a365a53048dd3ae964639e798d9742
Oguzhan1978 <74024518+Oguzhan1978@users.noreply.github.com>
2021-02-24 12:37:28 -0500

    added see module codes

---
+++ src/test/java/com/cybertek/step_definitions/Library_StepDefinition.java
@@ -0,0 +1,38 @@
+package com.cybertek.step_definitions;
+
+import com.cybertek.pages.LibraryMainPage;
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class Library_StepDefinition {
+
+
+    LibraryMainPage libraryMainPage = new LibraryMainPage();
+
+
+    @Given("the student on the home page")
+    public void the_student_on_the_home_page() {
+
+        String url = ConfigurationReader.getProperty("url");
+        Driver.getDriver().get(url);
+
+        libraryMainPage.loginLibrary();
+
+    }
+
+    @Then("the user should see following modules")
+    public void the_user_should_see_following_modules(List<String> expectedModules) {
+
+        System.out.println("ExpectedModulesList: " + expectedModules);
+
+        for (WebElement eachLink : libraryMainPage.moduleLinks) {
+            System.out.println("eachLink = " + eachLink.getText());
+        }
+    }
+
+}
